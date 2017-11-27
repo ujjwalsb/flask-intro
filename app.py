f3dc15e578e77bc0dd394b78de20ffe992d5d8b5
ujjwalsb <ujjwalsingh15@gmail.com>
2017-11-27 13:09:51 +0530

    adding loging page to the static website

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -6,9 +6,21 @@ app = Flask(__name__)
 def home():
 	return "Hello World"
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+			error = 'Invalid username or password, please try again !'
+		else:
+			return redirect(url_for('home'))
+	return render_template('login.html', error=error)
+	
 @app.route('/welcome')
 def welcome():
 	return render_template('welcome.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
+			
